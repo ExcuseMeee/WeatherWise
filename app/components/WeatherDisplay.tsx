5f15a7d43c942eeba1c5302cfb882bb02d96cb5f
Oscar Lin <linoscar01@gmail.com>
2023-05-20 23:29:01 -0400

    country code lookup

--- app/components/WeatherDisplay.tsx
+++ app/components/WeatherDisplay.tsx
@@ -1,5 +1,6 @@
 import { GeolocationData, Units, WeatherData } from "@/types";
 import Image from "next/image";
+import lookup from 'country-code-lookup'
 
 type ComponentProps = {
   geo: GeolocationData;
@@ -13,22 +14,23 @@ const WeatherDisplay = ({ geo, weather, units }: ComponentProps) => {
   }
 
   // show weather, pressure, humid, wind
+  // console.log(lookup.byIso(geo.country))
 
   return (
     <div className="border-2 border-black flex flex-col items-center">
       <div className="w-1/3" >
-        <span className="font-bold text-2xl">{geo.name}</span>
+        <p className="font-bold text-2xl">{geo.name}</p>
+        <p>{lookup.byIso(geo.country)?.country}, {lookup.byIso(geo.country)?.continent}</p>
       </div>
       <div className="flex flex-col">
-        <div className="flex justify-center -mt-8 -mb-14">
+        <div className="flex justify-center -mt-12 -mb-14">
           <Image
             src={`https://openweathermap.org/img/wn/${
               weather.weather[0]!.icon
             }@4x.png`}
             alt={"Weather Icon"}
             width={250}
-            height={225}
-            loading="eager"
+            height={250}
           />
         </div>
         <div className="text-center">
