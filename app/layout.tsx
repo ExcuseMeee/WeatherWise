67991919a078c33ee9d9c8b219554328704dd82a
Oscar Lin <linoscar01@gmail.com>
2023-06-10 18:50:30 -0400

    metadata changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,13 @@
+import { Metadata } from "next";
 import SearchBar from "./components/SearchBar";
 import "./globals.css";
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "WeatherWise",
   description: "Get the Latest Climate Data for any Location",
+  creator: "Oscar Lin",
+  keywords: ["weather", "climate", "nextjs", "react", "server components"],
+  authors: { name: "Oscar Lin", url: "https://www.linkedin.com/in/oscarlin01" },
 };
 
 export default function RootLayout({
